ff5fbd6e21e5941a5bc2ab8b43b3a8a453f482c6
hyunsoo <gustn1029@gmail.com>
2021-04-12 15:49:40 +0900

    'regexp-1day'

---
+++ main.js
@@ -0,0 +1,16 @@
+let str = `
+010-1234-5678
+gustn1029@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+aabbccddee
+`;
+
+// const regexp = new RegExp('the','gi');
+const regexp = /the/gi
+// console.log(str.match(regexp));
+// console.log(regexp.test(str));
+// str = str.replace(regexp, 'AAA')
+// console.log(str);
+
+console.log(str.match(/\.$/gim))
